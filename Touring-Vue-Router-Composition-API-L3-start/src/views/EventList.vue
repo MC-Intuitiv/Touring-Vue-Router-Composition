dd999a181c6a99bfabdf7a06a4cc97cbb5b26f4f
Michael Cooper <michael.cooper@intuitiv.net>
2025-09-05 14:24:37 +0100

    pagination functionality added

--- Touring-Vue-Router-Composition-API-L3-start/src/views/EventList.vue
+++ Touring-Vue-Router-Composition-API-L3-start/src/views/EventList.vue
@@ -1,25 +1,51 @@
 <script setup>
 import EventCard from "@/components/EventCard.vue";
 import EventService from "@/services/EventService.js";
-import { onMounted, ref } from "vue";
+import { onMounted, ref, computed, watchEffect } from "vue";
 
-const events = ref(null);
+const props = defineProps(['page'])
+
+const events = ref("");
+
+const page = computed(() => props.page)
+
+const totalEvents = ref(0)
+
+
+const hasNextPage = computed(() => {
+  // Calculate totalPages, based on 2 per page
+  const totalPages = Math.ceil(totalEvents.value / 2)
+
+  // If current page is less than total pages, return true
+  return page.value < totalPages
+})
 
 onMounted(() => {
-  EventService.getEvents()
-    .then((response) => {
-      events.value = response.data;
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-});
+  watchEffect(() => {
+    events.value = null
+    EventService.getEvents(2, page.value)
+      .then(response => {
+        events.value = response.data
+        // our response has total stored in the header.
+        totalEvents.value = response.headers['x-total-count']
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  })
+})
 </script>
 
 <template>
   <h1>Events for Good</h1>
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+
+    <router-link :to="{ name: 'EventList', query: { page: page - 1 } }" rel="prev" v-if="page != 1">Prev
+      Page</router-link>
+
+    <router-link :to="{ name: 'EventList', query: { page: page + 1 } }" rel="next" v-if="hasNextPage">Next
+      Page</router-link>
   </div>
 </template>
 
